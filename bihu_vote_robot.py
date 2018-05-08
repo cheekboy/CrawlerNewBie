7418b42e0ccb2193554918523f9b2f206702fe7f
sd1700092 <foreverskyone@hotmail.com>
2018-05-08 22:34:07 +0800

    commit upvote

--- bihu_vote_robot.py
+++ bihu_vote_robot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+import json
 import ssl
 from urllib import parse, request
 
@@ -22,8 +23,31 @@ def getUserArtList(queryUserId):
     data = parse.urlencode(data).encode('utf-8')
     req = request.Request(url, headers=headers, data=data)
     rsp = request.urlopen(req, context=context).read()
+    # rsp = request.urlopen(req).read()
     rsp = rsp.decode('utf-8')
     return rsp
 
+def check_need_up(body):
+    data_str=json.loads(body)
+    res = data_str['res']
+    need_up = False
+    artId = 0
+    is_succ = False
+    log_content_format = '【%s】的最新文章已有【%d】赞，文章收益【¥%.2f】，文章标题【%s】'
+    if res == 1 and data_str["data"]["total"] > 0:
+        item=data_str["data"]['list'][0]
+        log_content = log_content_format % (item['userName'], item['ups'], item['money'], item['title'])
+        print(log_content)
+        if item['up'] == 0 and item['ups'] < 100:
+            need_up = True
+        artId = item['id']
+
+    if res==1:
+        is_succ = True
+
+    return is_succ, need_up, artId
+
 if __name__ == "__main__":
-    print(getUserArtList(13679))
+    body = getUserArtList(13679)
+    print (body)
+    print(check_need_up(body))
